4051df62fdc1d6f4c1196d7a3fa4e8b86ceeec38
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-06-24 17:24:45 +0300

    reimplement using gspread

--- gpu_police/sheets_middleware.py
+++ gpu_police/sheets_middleware.py
@@ -1,52 +1,18 @@
-import pickle
-import os.path
-import sys
-import collections
-import datetime
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 from pathlib import Path
+
+import gspread
 
 from gpu_police.config import config
 
-SCOPES = config.whitelist.scopes
-SPREADSHEET_ID = config.whitelist.spreadsheet_id
-TOKEN_FILE = Path(config.whitelist.token_file).expanduser()
+gc = gspread.service_account(Path(config.whitelist.credentials).expanduser())
 
-def update_token():
-    creds_file = Path(config.whitelist.credentials).expanduser()
-    flow = InstalledAppFlow.from_client_secrets_file(creds_file, SCOPES)
-    creds = flow.run_local_server(port=0)
-    with open(TOKEN_FILE, 'wb') as token:
-        pickle.dump(creds, token)
-
-def _get_values(query):
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open(TOKEN_FILE, 'rb') as token:
-            creds = pickle.load(token)
-
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-            with open(TOKEN_FILE, 'wb') as token:
-                pickle.dump(creds, token)
-        else:
-            update_token()
-
-    service = build('sheets', 'v4', credentials=creds)
-    sheet = service.spreadsheets()
-
-    result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range=query).execute()
-    names = result.get('values', [])
-
-    return names
+def _get_values(sheet, query):
+    sh = gc.open(config.whitelist.table_name)
+    return sh.worksheet(sheet).get(query)
 
 def get_whitelist():
     result = []
-    for query in config.whitelist.queries:
-        result += _get_values(query)
+    for item in config.whitelist.queries.values():
+        result += _get_values(item.sheet, item.query)
     result = [i[0] for i in result if i]
-
     return result
