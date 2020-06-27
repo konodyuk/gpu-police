99210d413c209ec52bf984e49fc7cf7d09e01f7f
Nikita Konodyuk <nikita.konodyuk@gmail.com>
2020-06-27 12:05:46 +0300

    adjust timestamp regex to support both current and legacy log styles

--- gpu_police/logging.py
+++ gpu_police/logging.py
@@ -3,7 +3,7 @@ import re
 from rich.console import Console
 from rich.markup import escape
 
-TIMESTAMP_REGEX = "\[\[\d{2}:\d{2}:\d{2}\]\]"
+TIMESTAMP_REGEX = "\[\[[\d\.: ]+\]\]"
 
 def get_console(out_file=None):
     return Console(log_path=False, width=125, highlight=False, tab_size=16, file=out_file, log_time_format="[%d.%m.%y %H:%M]")
